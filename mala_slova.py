e381beddeb9fbeca7e8a546f22048eb4867ae56b
ljpetkovic <42590315+ljpetkovic@users.noreply.github.com>
2019-06-05 12:38:00 +0200

    Add files via upload

---
+++ mala_slova.py
@@ -0,0 +1,8 @@
+os.chdir('/Users/ljudmilapetkovic/Desktop/Preprocesirani_tekstovi')     
+
+import fileinput
+import glob
+import sys
+
+for line in fileinput.input(glob.glob('*.txt'), inplace=True):
+    sys.stdout.write(line.lower())
